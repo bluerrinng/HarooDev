95a1b218ecc7d15caf5c3b3872d4fa617cffb9f8
JunSoo Kim <jun.soo1828@gmail.com>
2025-07-21 23:33:21 +0900

    Update 10.cpp

--- DevNotes/Problems/10.cpp
+++ DevNotes/Problems/10.cpp
@@ -19,4 +19,53 @@
 
 */
 
-// 문제 분석 참고: https://github.com/bluerrinng/HarooDev/blob/main/DevNotes/Explanation/10.md
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<char, char> bracketPair = {
+    {')', '('},
+    {']', '['},
+    {'}', '{'}
+};
+
+
+bool isValid(string&s , int start)
+{
+    stack<char> stk;
+    unsigned int sz = s.size();
+
+    for(int i = 0; i<sz; i++)
+    {
+        char ch = s[(start+i)%sz];
+
+        if(bracketPair.count(ch))
+        {
+            if(stk.empty() || stk.top() != bracketPair[ch])
+            {
+                return false;
+            }
+            stk.pop();
+        } else
+        {
+            stk.push(ch);
+        }
+    }
+
+    return stk.empty();
+}
+
+int solution(string s)
+{
+    int answer = 0;
+    int n = s.size();
+
+    for(int i = 0; i < n ; i++)
+    {
+        answer += isValid(s,i);
+    }
+
+    return answer;
+}
