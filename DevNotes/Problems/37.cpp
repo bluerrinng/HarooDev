b3a42c6008925d227e3c6da4e3aec565d9398d33
JunSoo Kim <jun.soo1828@gmail.com>
2025-08-07 01:17:54 +0900

    Create 37.cpp

---
+++ DevNotes/Problems/37.cpp
@@ -0,0 +1,40 @@
+
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+unordered_map<char, vector<char>> neighbors;
+vector<char> result;
+unordered_set<char> visited;
+
+void dfs(char node)
+{
+    visited.insert(node);
+    result.push_back(node);
+
+    for(auto ch : neighbors[node])
+    {
+        if(visited.find(ch) == visited.end())
+        {
+            dfs(ch);
+        }
+    }
+}
+
+vector<char> solution(vector<pair<char,char>> graph, char start)
+{
+    for(const auto& v : graph)
+    {
+        char firstv = v.first;
+        char secondv = v.second;
+
+        neighbors[firstv].push_back(secondv);
+    }
+
+    dfs(start);
+    return result;
+}
