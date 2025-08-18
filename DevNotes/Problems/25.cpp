9bc3f22844b4a4110c0d7abd5f6cb8f170c4d8cd
JunSoo Kim <jun.soo1828@gmail.com>
2025-08-18 23:49:41 +0900

    Create 25.cpp

---
+++ DevNotes/Problems/25.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> genres, vector<int> plays)
+{
+    unordered_map<string, int> totalCount;
+
+    for(int i =0 ;i<genres.size();i++)
+    {
+        totalCount[genres[i]] = 
+    }
+}
