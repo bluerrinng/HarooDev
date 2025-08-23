0121d25913a43d22924a9d03eca733e2997b0b8c
JunSoo Kim <jun.soo1828@gmail.com>
2025-08-23 22:14:30 +0900

    Update 25.cpp

--- DevNotes/Problems/25.cpp
+++ DevNotes/Problems/25.cpp
@@ -1,22 +1,34 @@
+/*
+25.cpp
+스트리밍 사이트에서 장르별로 가장 많이 재생된 노래를 2개씩 모아 베스트 앨범을 출시하려 한다. 
+노래는 고유 번호로 구분하며, 노래 수록 기준은 다음과 같다:
+- 속한 노래가 많이 재생된 장르를 먼저 수록
+- 장르 내에서 많이 재생된 노래를 먼저 수록
+- 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록 
+
+그래서 고유 번호 순서대로 반환하는 solution() 함수 
+*/
+
+#include <algorithm>
 #include <iostream>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-//다른 장르끼리 비교해서 어떤 장르가 더 많이 재생되었는지 확인용 정렬
-bool compareGenre(const pair<string,int>&a , const pair<string,int> &b)
+
+//다른 장르끼리 비교할때 더 많이 재생된 순으로 정렬
+bool sortTotalplays(const pair<string,int>& a, const pair<string, int>& b)
 {
     return a.second > b.second;
 }
 
-
-//같은 장르내에서 어떤 인덱스가 더 많이 재생되었는지 확인용
-bool compareSong(const pair<int,int>&a, const pair<int,int>&b)
+//같은 장르내에서 더 많이 재생된 음악의 고유 번호 순으로 정렬
+bool sortSameType(const pair<int,int>& a, const pair<int,int>&b)
 {
     if(a.second == b.second)
     {
-        return a.first < b.first;
+        return a.first < b.second;
     }
     else
     {
@@ -24,37 +36,33 @@ bool compareSong(const pair<int,int>&a, const pair<int,int>&b)
     }
 }
 
-vector<int> solution(vector<string>genres, vector<int> plays)
+vector<int> solution(vector<string> genres, vector<int> plays)
 {
     vector<int> result;
+    unordered_map<string, vector<pair<int,int>>> gpair;
+    unordered_map<string,int> totalNum;
 
-    unordered_map<string, vector<pair<int,int>>> genre_map;
-    unordered_map<string, int> song_compare_map;
-
-    for(int i = 0; i< genres.size();i++)
+    //각각의 장르의 총 재생횟수를 구해서 맵으로 만들기
+    for(int i = 0; i<genres.size();i++)
     {
-        genre_map[genres[i]].push_back({i,plays[i]});;
-        song_compare_map[genres[i]] += plays[i];
+        totalNum[genres[i]] += plays[i];
+        gpair[genres[i]].push_back({i,plays[i]});
     }
 
-    vector<pair<string,int>> sortedGenres (song_compare_map.begin(), song_compare_map.end());
+    vector<pair<string,int>> sortg (totalNum.begin(),totalNum.end());
+    sort(sortg.begin(),sortg.end(),sortTotalplays);
 
-    sort(sortedGenres.begin(),sortedGenres.end(),compareGenre);
-
-    for(auto& val : sortedGenres)
+    for(auto& v: sortg)
     {
-        auto& songs = genre_map[val.first];
-        sort(songs.begin(),songs.end(),compareSong);
+        auto& genreVector = gpair[v.first];
+        sort(genreVector.begin(),genreVector.end(),sortSameType);
 
-
-        for(int i =0 ; i< min(2,(int)songs.size());i++)
+        for(int i =0 ; i< min(2,(int)genreVector.size());i++)
         {
-            result.push_back(songs[i].first);
+            result.push_back(genreVector[i].first);
         }
     }
 
     return result;
 }
-
-
 
