d347b3d566ea2552ba0633e67a6ca572f9ab28ae
JunSoo Kim <jun.soo1828@gmail.com>
2025-08-27 23:41:01 +0900

    Update 2468.cpp

--- DevProblems/acmipc/2468.cpp
+++ DevProblems/acmipc/2468.cpp
@@ -1,38 +1,35 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 
 using namespace std;
 
-int dx[4] = {0,1,0,-1};
-int dy[4] = {-1,0,1,0};
-
-unordered_set<string> result;
-
-void dfs(int x, int y, vector<vector<int>> grid)
+int findMax(vector<vector<int>> grid)
 {
-    int cnt = 0; 
-    for(int i = 0; i < 4; i++)
-    {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    int max = 0;
 
-        for(int j = 0; j<4; j++)
-        {
-            dfs(nx,ny,grid);
-            cnt++;
-        }
+    for(int i = 0; i < grid.size();i++)
+    {
+        
     }
-     
 }
 
-int solution(vector<vector<int>> grid)
+int main()
 {
-    for(int i = 0; i<5;i++)
+    int N;
+    cin >> N;
+
+    vector<vector<int>> grid(N,vector<int>(N,0));
+
+    for(int i = 0; i < N; i++)
     {
-        for(int j =0; j<5; j++)
+        for(int j = 0; j< N; j++)
         {
-            dfs(i,j,grid);
+            int a;
+            cin >> a;
+            grid[i][j] = a;
         }
     }
+
+
 }
