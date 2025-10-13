def1630f63bc4921cdd37adb040506438d94d7ab
JunSoo Kim <jun.soo1828@gmail.com>
2025-10-14 00:11:39 +0900

    Create Knapsack.cpp

---
+++ data-structure/Chapter 12_BackTracking/Knapsack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int>& weights, vector<int>& values ,int maxWeight)
+{
+    int sum = 0 ;
+}
+
+
+int main()
+{
+    int number = 4;
+    int weightMax = 7;
+
+    vector<int> weights = {6,4,3,5};
+    vector<int> values = {13,8,6,12};
+
+    cout << solution(weights, values, weightMax);
+
+    return 0;
+}
